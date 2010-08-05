b7906dd0a0cf6098ba0428e9fb4a19be660a9661
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-08-05 17:57:25 +0100

    Use File.join with Rails.root

--- initializers/load_site_config.rb
+++ initializers/load_site_config.rb
@@ -1,2 +1,2 @@
-raw_config = File.read(Rails.root + "/config/site_config.yml")
+raw_config = File.read(File.join(Rails.root, "config/site_config.yml"))
 SITE_CONFIG = YAML.load(raw_config).symbolize_keys
